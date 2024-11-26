b33b9b3baa80b6629b06f6893744f569bba62df9
Vasiliy <vassiliyprm@gmail.com>
2024-11-26 15:38:22 +0500

    Загрузка фото с сайта.

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import  BytesIO
+
+from pygame.examples.aliens import load_image
+
+window = Tk()
+window.title("Cats")
+window.geometry("600x400")
+
+label= Label()
+label.pack()
+
+url = "https://cataas.com/cat"
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
